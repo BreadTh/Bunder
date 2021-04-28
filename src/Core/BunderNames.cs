7d21ae4f15fb965ce3c3f8d33748d96e89c23730
Chris Jensen@any.cloud <cje@anycloud.dk>
2021-04-28 13:12:58 +0200

    correct naming

--- src/Core/BunderNames.cs
+++ src/Core/BunderNames.cs
@@ -23,13 +23,13 @@
         internal string DisplayName => setName;
 
         
-        internal string SharedLog => "_bunder.log";
-        internal string SharedNew => "_bunder.new";
-        internal string SharedRetry => "_bunder.retry";
-        internal string SharedResume => "_bunder.resume";
-        internal string SharedReject => "_bunder.reject";
-        internal string SharedRevive => "_bunder.revive";
-        internal string SharedCompleted => "_bunder.completed";
+        internal string SharedLog => "bunder.log";
+        internal string SharedNew => "bunder.new";
+        internal string SharedRetry => "bunder.retry";
+        internal string SharedResume => "bunder.resume";
+        internal string SharedReject => "bunder.reject";
+        internal string SharedRevive => "bunder.revive";
+        internal string SharedCompleted => "bunder.completed";
         
     }
 }
